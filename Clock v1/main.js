b0568da638fc5af150b0ccf63ae0afbc9c038c49
Mr Akshay Shinde <107102876+MrAkshayAS@users.noreply.github.com>
2023-03-03 15:45:48 +0530

    v1.0.3
    Adding new features and responsiveness

--- Clock v1/main.js
+++ Clock v1/main.js
@@ -9,21 +9,34 @@ function displayTime() {
   function btn1() {
     if (founder.style.display != "block") {
       founder.style.display = "block";
+      about.style.display = "none";
+      source.style.display = "none";
     } else {
       founder.style.display = "none";
+      // about.style.display = "block";
+      // source.style.display = "block";
+      
     }
   }
   function btn2() {
     if (about.style.display != "block") {
       about.style.display = "block";
+      source.style.display = "none";
+      founder.style.display = "none";
     } else {
       about.style.display = "none";
+      // founder.style.display = "block";
+      // source.style.display = "block";
     }
   }
   function btn3() {
     if (source.style.display != "block") {
       source.style.display = "block";
+      about.style.display = "none";
+      founder.style.display = "none";
     } else {
       source.style.display = "none";
+      // about.style.display = "block";
+      // founder.style.display = "block";
     }
   }
